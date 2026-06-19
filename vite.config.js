bf8f889766f538398f80822995cd5379f57953c9
Jonathan Spanos <jonspanos04@outlook.com>
2026-06-19 13:03:27 -0400

    Updated vite build configuration to include TOS and privacy policy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        tos: 'tos.html',
+        privacy_policy: 'privacy-policy.html'
+      }
+    },
+  },
   envDir: './',
   server: {
     allowedHosts: [
